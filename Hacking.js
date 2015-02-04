57ab02f1e634f6cde1029217743f4059da02b835
GreenCyan <oysong@ucsc.edu>
2015-02-03 16:26:26 -0800

    index.html added, Hacking.js modified a little

--- Hacking.js
+++ Hacking.js
@@ -1,6 +1,7 @@
 var command = new TextBox("Type commands here");
 command.x = 200;
 command.y = 600;
+command.width = 500;
 command.fontSize = 12;
 command.clearOnFocus = true;
 command.editable = true;
